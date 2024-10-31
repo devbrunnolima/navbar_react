8632ae078f247bba9c2246379f4630d742427fc8
devbrunnolima <devbrunnolima@gmail.com>
2024-10-31 17:21:45 -0300

    feat: novas paginas para as rotas da navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,63 +1,72 @@
 "use client";
 
-import Link from "next/link";
 import Image from "next/image";
-import Navitem, { NavItemInterface } from "../Navitem";
-import "./index.css";
+import Link from "next/link";
+import NavItem, { NavItemInterface } from "../NavItem";
+import "./index.css"
 import { usePathname } from "next/navigation";
+import { FaBars, FaXmark } from "react-icons/fa6";
+import { useState } from "react";
 
 export default function Navbar() {
-  const items: NavItemInterface[] = [
-    {
-      url: "/",
-      label: "Início",
-    },
-    {
-      url: "/about",
-      label: "Sobre",
-    },
-    {
-      url: "/products",
-      label: "Produtos",
-    },
-    {
-      url: "/services",
-      label: "Serviços",
-    },
-    {
-      url: "/contact",
-      label: "Contatos",
-    },
-  ]
+    const items: NavItemInterface[] = [
+        {
+            url: "/",
+            label: "Início"
+        },
+        {
+            url: "/about",
+            label: "Sobre"
+        },
+        {
+            url: "/products",
+            label: "Produtos"
+        },
+        {
+            url: "/services",
+            label: "Serviços"
+        },
+        {
+            url: "/contacts",
+            label: "Contatos"
+        },
+    ]
 
-  const pathname = usePathname();
+    const pathname = usePathname();
 
-  return (
-    <header>
-      <nav className="navbar">
-        <Link href="/" className="logo">
-          <Image 
-            src="/logonovo.svg"
-            width={50}
-            height={50}
-            alt={"Logo do sistema"}
-          />
-        </Link>
-        <ul className="nav-items">
-          {items.map((item, index) => (
-            <Navitem 
-               key={index}
-               url={item.url} 
-               label={item.label}
-               isActive={pathname === item.url}
-             />
-          ))}
-        </ul>
+    const [openMenu, setOpenMenu] = useState<boolean>(false);
 
-        <button className="btn-default">
-            Contatar
-        </button>
-      </nav>
-    </header>
-  );
+    return (
+        <header>
+            <nav className="navbar">
+                <Link href="/" className="logo">
+                    <Image 
+                        src="logo.svg" 
+                        width={50}
+                        height={50} 
+                        alt="Logo do sistema"
+                    />
+                </Link>
+
+                <ul className={`nav-items ${openMenu ? 'open' : ''}`}>
+                    {items.map((item, index)=> (
+                        <NavItem
+                            key={index}
+                            url={item.url}
+                            label={item.label}
+                            isActive={pathname === item.url}
+                        />
+                    ))}
+                </ul>
+
+                <button className="btn-mobile" onClick={() => setOpenMenu(!openMenu)}>
+                    {openMenu ? <FaXmark /> : <FaBars />}
+                </button>
+
+                <button className="btn-default">
+                    Contatar
+                </button>
+            </nav>
+        </header>
+    );
 }
--- src/components/Navitem/index.tsx
+++ src/components/Navitem/index.tsx
@@ -2,17 +2,19 @@ import Link from "next/link";
 import "./index.css";
 
 export interface NavItemInterface {
-    url: string; 
-    label:string;
-    isActive?: boolean; 
+    url: string;
+    label: string;
+    isActive?: boolean;
 }
 
-export default function Navitem(props: NavItemInterface) {
-  return (
-    <li className="nav-item">
-      <Link href={props.url} className={`nav-link ${props.isActive ? 'active' : ''}`}>
-        {props.label}
-      </Link>
-    </li>
-  );
+export default function NavItem(props: NavItemInterface) {
+    return(
+        <li className="nav-item">
+            <Link 
+                href={props.url} 
+                className={`nav-link ${props.isActive ? 'active' : ''}`}>
+                {props.label}
+            </Link>
+        </li>
+    );
 }
